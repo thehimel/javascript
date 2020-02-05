bb41ba43e9b4efdcd7bddd109ab6a16cc8a42cf2
Himel Das <himelhub@gmail.com>
2020-02-06 04:02:36 +0600

    2-11

--- 2-10-challenge1.js
+++ 2-10-challenge1.js
@@ -3,7 +3,7 @@ Mark and John are trying to compare their BMI (Body Mass Index), which is calcul
 BMI = mass / height^2 (mass in kg and height in meter).
 
 1. Store the masses and heights of Mark and John in variables.
-2. Calculate their BMIs
+2. Calculate their BMIs.
 3. Create a boolean variable containing information about whether Mark has a higher BMI than John.
 4. Print a string to the console containing the variable from step 3.
 (Something like "Is Mark's BMI higher than Jokhn's? true/false")
@@ -15,5 +15,5 @@ heightMark = 1.8288;    //6ft=1.8288m
 massJohn = 90;
 heightJohn = 1.9812;    //6.5ft=1.9812m
 
-isMarkHigherBMI = massMark/heightMark**2 > massJohn/heightJohn**2;   //Comparison operator > has the lowest precendence here
-console.log('Is Mark\'s BMI higher than Jokhn\'s? '+ isMarkHigherBMI);
+isMarkHigherBMI = massMark/heightMark**2 > massJohn/heightJohn**2;   //Comparison operator '>' has the lowest precendence here
+console.log('Is Mark\'s BMI higher than John\'s? '+ isMarkHigherBMI);
---
+++ 2-11-if-else.js
@@ -0,0 +1,9 @@
+let a=20, b=10;
+
+if(a>b){
+    console.log('a is larger');
+} else if(a===b){
+    console.log('both are equal');
+} else{
+    console.log('b is larger');
+}
